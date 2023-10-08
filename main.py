d9e709ed5d5754b4118f6b670fa879b9d9143524
Sasha <alexsandernikolaevs@gmail.com>
2023-10-08 17:37:37 +0300

    first-commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import PyQt5 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+app = QApplication([])
+mw = QWidget()
+mw.show()
+
+app.exec_()
+
+
+
+
+
+
+
+
+
+
